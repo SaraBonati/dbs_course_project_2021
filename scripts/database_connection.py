61fa1119c191777f28c9671cd3b31bc89076c6ce
fu5247mt <fu5247mt@mi.fu-berlin.de>
2021-06-29 05:25:38 +0000

    Add scripts for creating a new database, tables, and populating them

--- scripts/database_connection.py
+++ scripts/database_connection.py
@@ -1,8 +1,8 @@
-# This script defines all functions/classes concernign connection to the 
+# This script defines all functions/classes concernign connection to the
 # PostgreSQL client PgAdmin4.
 
 # Group: Sara Bonati - Elica Tokmakchieva - Tobias Sandmann
-#---------------------------------------------------------------------------
+# ----------------------------------------------------------------------
 
 # general utility import
 import numpy as np
@@ -13,33 +13,42 @@ import psycopg2
 from psycopg2.extensions import register_adapter, AsIs
 psycopg2.extensions.register_adapter(np.int64, psycopg2._psycopg.AsIs)
 
+
 # define database class to handle connections and queries
 class DB():
-    def __init__(self,ddir):
+    def __init__(self, ddir):
         """
         This function initializes a DB (database) object, specifically
         to handle connection to PostgreSQL server.
-        
+
         Inputs:
         - ddir (string): path to data files
         Outputs:
         - None
 
         """
-        #location of data files
-        self.ddir= ddir
-        #Connect to an existing database
-        self.conn = psycopg2.connect("dbname=test user=postgres password=assign9 port=5432")
+        # Location of data files
+        self.ddir = ddir
+        # Connect to an existing database
+        self.conn = psycopg2.connect(
+                dbname="db_project_2021",
+                user="root",
+                password="root",
+                host="localhost",
+                port="5432"
+        )
+
         # Open a cursor to perform database operations
         self.cur = self.conn.cursor()
 
     def create_tables(self):
         """
-        This function creates tables in the "final_project" PostgreSQL database according to
-        the specificed ER model and relational model. After creation, the tables are populated
-        with data from the final "cleaned" csv files, using the INSERT SQL command.
-        Changes are committed to PgAdmin4, so that newly created tables can be seen also in there.
-        
+        This function creates tables in the "final_project" PostgreSQL database
+        according to the specificed ER model and relational model. After
+        creation, the tables are populated with data from the final "cleaned"
+        csv files, using the INSERT SQL command.  Changes are committed to
+        PgAdmin4, so that newly created tables can be seen also in there.
+
         Inputs:
         - data_files (string): path to data files
         - sql_queries ()
@@ -47,70 +56,63 @@ class DB():
         - None
 
         """
-        self.co2_table = pd.read_csv(os.path.join(self.ddir,'final','co2_final.csv'))
-        self.gdp_table = pd.read_csv(os.path.join(self.ddir,'final','gdp_final.csv'))
 
         try:
-            # create table 
+            # create table
             self.cur.execute("""
-                            CREATE TABLE assignment (
-                            date INTEGER PRIMARY KEY,
-                            low INTEGER NOT NULL,
-                            high INTEGER NOT NULL,
-                            endday INTEGER NOT NULL,
-                            volume INTEGER NOT NULL
-                            )
-                            """)
+                   CREATE TABLE co2(
+                       date DATE PRIMARY KEY,
+                       low INTEGER NOT NULL,
+                       high INTEGER NOT NULL,
+                       endday INTEGER NOT NULL,
+                       volume INTEGER NOT NULL
+                   );
+            """)
 
             print("Tables created!")
-        
-        except Exception as error:
-            print("An error has occured in creating data tables, see: ",error)
-            print ("Exception TYPE:", type(error))
-        
-        
-        try:
-            # fill table with data from csv file
-            for i in range(len(self.co2_table)):
-                self.cur.execute("INSERT INTO assignment (date,low,high,endday,volume) VALUES (%s, %s, %s, %s, %s)",(self.co2_table.loc[i,'Datum'],
-                                                                                                                     self.co2_table.loc[i,' Tief'],
-                                                                                                                     self.co2_table.loc[i,' Hoch'],
-                                                                                                                     self.co2_table.loc[i,' Tagesendwert'],
-                                                                                                                     self.co2_table.loc[i,' Handelsvolumen']))
 
-            
-            print("Tables filled with data!")
-        
-        except Exception as error:
-            print("An error has occured in filling data tables, see: ",error)
-            print ("Exception TYPE:", type(error))
-        
-        # commit changes to pgadmin4 (so that you can see newly created table there)
+        except Exception as e:
+            print("An error has occured in creating data tables, see: ", e)
+            print("Exception TYPE:", type(e))
+
+        co2_csv = os.path.join(self.ddir, 'final', 'co2_final.csv')
+
+        # Copy from the csv file into the 'assignment' table
+        with open(co2_csv, 'r') as f:
+            next(f)  # Skip header row
+            self.cur.copy_from(f, 'co2', sep=',')
+
         self.conn.commit()
 
-
-
-    def convert_query_to_sql(self,q):
+    def convert_query_to_sql(self, q):
         """
-        This function converts a Python string/object data type in 
+        This function converts a Python string/object data type in
         appropriate
-        
+
         Inputs:
-        - q: 
+        - q:
         Outputs:
         - None
         """
         # Pass data to fill a query placeholders and let Psycopg perform
         # the correct conversion (no more SQL injections!)
-        self.cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",(100, "abc'def"))
-        
-        # define queries of interest (e.g. ....)
-        self.query_fig1 = "SELECT * FROM test"
+        number = 100
+        text = 'foo'
+        self.cur.execute(f"""
+                INSERT INTO test (num, data) VALUES ({number}, {text})")
+        """)
 
-        # Query the database and obtain data as pandas dataframe (simpler for our purposes)
+        # define queries of interest (e.g. ....)
+        self.query_fig1 = f"""
+                          SELECT *
+                          FROM test
+                          WHERE name = '{text}'
+                          """
+
+        # Query the database and obtain data as pandas dataframe (simpler for
+        # our purposes)
         res = pd.read_sql_query(self.query_fig1, self.conn)
 
-    
     def close_connection(self):
         """
         This function terminates connection to DB in PgAdmin4
@@ -124,5 +126,3 @@ class DB():
         self.cur.close()
         self.conn.close()
         print("Database connection closed!")
-
-
---
+++ scripts/table_population.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT as autocommit
+
+
+def populate_tables(dbname):
+    """
+    Populate PostgreSQL tables with data from CSV files
+    """
+    filenames = [
+            "COUNTRY",
+            "POPULATION",
+            "GDP",
+            "HEALTH",
+            "EDUCATION",
+            "POLLUTION",
+            "SECURITY"]
+    try:
+        # Connect to PostgreSQL server
+        connection = psycopg2.connect(
+                dbname=dbname,
+                user="postgres",
+                password="postgres",
+                host="localhost",
+                port="5432")
+
+        print(f"\nConnected with database {dbname}")
+        connection.set_isolation_level(autocommit)
+
+        cursor = connection.cursor()
+
+        # Populate all tables one after the other
+        for filename in filenames:
+            with open(f'../data/final/{filename}.csv', 'r') as f:
+                cursor.copy_expert(
+                        f"COPY {filename.lower()} FROM STDIN WITH HEADER CSV",
+                        f)
+            print(f"Table {filename.lower()} populated.")
+
+        print("All done!")
+    except (Exception, psycopg2.Error) as error:
+        print(error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+
+
+if __name__ == "__main__":
+    populate_tables("dbs_project")
